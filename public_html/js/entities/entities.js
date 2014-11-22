4051533f426bd946e0133dbef038c573d8cdf383
Nicholas Malfitano <nicholas1malfitano@gmail.com>
2014-11-21 17:37:58 -0800

    vid 14

--- public_html/js/entities/entities.js
+++ public_html/js/entities/entities.js
@@ -7,7 +7,7 @@ game.PlayerEntity = me.Entity.extend({
         width: 128,
         height: 128,
         getShape: function(){
-        return (new me.Rect(0, 0, 128, 128)).toPolygon();
+        return (new me.Rect(0, 0, 30, 128)).toPolygon();
         }
    }]);
    
@@ -17,6 +17,8 @@ game.PlayerEntity = me.Entity.extend({
         this.renderable.setCurrentAnimation("idle");
   
         this.body.setVelocity(5, 20);
+        me.game.viewport.follow(this.pos, me.game.viewport.AXIS.BOTH);
+        
         },
         update: function(delta) {
         if (me.input.isKeyPressed("right")){
@@ -49,16 +51,19 @@ game.PlayerEntity = me.Entity.extend({
 });
 
 
-        game.levelTrigger = me.Entity.extend({
+        game.LevelTrigger = me.Entity.extend({
         init: function(x, y, settings){ 
             this._super(me.Entity, 'init', [x, y, settings]);
             this.body.onCollision = this.onCollision.bind(this);
             this.level = settings.level;
+            this.xSpawn = settings.xSpawn;
+            this.ySpawn = settings.ySpawn;
         },
 
         onCollision: function (){
-            this.body.onCollisionMask(me.collision.types.NO_OBJECT);
-            me.levelDirector.addLevel(this.level);
+            this.body.setCollisionMask(me.collision.types.NO_OBJECT);
+            me.levelDirector.loadLevel(this.level);
+            me.state.current().resetPlayer(this.xSpawn, this.ySpawn);
         }
 
         });
--- public_html/js/screens/play.js
+++ public_html/js/screens/play.js
@@ -8,8 +8,7 @@ game.PlayScreen = me.ScreenObject.extend({
                 
             me.levelDirector.loadLevel("mylevel01");
             
-            var player = me.pool.pull("mario", 0, 420, {});
-            me.game.world.addChild(player, 3);
+            this.resetPlayer(0, 400);
             
             me.input.bindKey(me.input.KEY.D, "right");
 
@@ -25,5 +24,11 @@ game.PlayScreen = me.ScreenObject.extend({
 	onDestroyEvent: function() {
 		// remove the HUD from the game world
 		me.game.world.removeChild(this.HUD);
-	}
+	},
+        
+        resetPlayer: function(x, y){
+            var player = me.pool.pull("mario", x, y, {});
+            me.game.world.addChild(player, 30);
+        }
+        
 });
